aec66f518aef529f725e98ae51ccd5c42f2579d8
VictorMac <vkobiletzkiy@gmail.com>
2017-01-26 14:34:43 +0300

    html name validation fixed

--- js/index.js
+++ js/index.js
@@ -222,7 +222,7 @@ function editNodeText(regex, input, helpId, helpMessage) {// See if the visitor 
 function isTheFieldEmpty(inputField, helpId) {// inputField – ID Number for the html text box
 // helpId – ID Number for the child node I want to print a warning in
 // See if the input value contains any text
-    return editNodeText(/^[A-Za-z\.\' \-]{2,15}\s?[A-Za-z\.\' \-]{2,15}\s?[A-Za-z\.\' \-]{2,15}/, inputField.value, helpId, "Please enter a valid name.");
+    return editNodeText(/^[A-Za-z\.\' \-]{2,15}\s?[A-Za-z\.\' \-]{2,15}\s?[A-Za-z\.\' \-]{2,15}/, inputField.value, helpId, "Please enter a valid first and last name.");
 }// inputField.value – Value typed in the html text box
 
 
